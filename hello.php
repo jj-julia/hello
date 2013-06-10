4cd689fec1359e8522b95fe23b1136ea2b166648
Julia Wells <wells.julia@gmai.com>
2013-06-10 18:44:05 +0800

    Preparing to tag version 0.1

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 	<title>Hello World</title>
 </head>
 <body>
-<h1>Hello</h1>
-<?php echo "My name is JJ";?>
+<h1>Hello - a new version</h1>
+<?php echo "My name is JJ the great";?>
 </body>
 </html>
